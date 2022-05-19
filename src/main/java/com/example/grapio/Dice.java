0a3a05e84342c54a4ac63248a43f1cdfa4f81524
Mateusz Dregier <235856@edu.p.lodz.pl>
2022-05-19 11:00:45 +0200

    poprawki tlo, kostka

--- src/main/java/com/example/grapio/Dice.java
+++ src/main/java/com/example/grapio/Dice.java
@@ -2,14 +2,13 @@ package com.example.grapio;
 import java.util.Random;
 
 public class Dice {
-    private int from, to;
     Random random;
 
     public Dice() {
         this.random = new Random();
     }
 
-    int giveRandom() {
+    int giveRandom(int from, int to) {
         return random.nextInt(from, to + 1);
     }
 }
