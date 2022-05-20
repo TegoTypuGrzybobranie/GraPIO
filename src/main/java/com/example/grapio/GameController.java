ebf39ed57d57aa792aab7c3ce06bf1a85987a52e
Dregierm <235856@edu.p.lodz.pl>
2022-05-20 15:05:05 +0200

    Ekran opcji

--- src/main/java/com/example/grapio/GameController.java
+++ src/main/java/com/example/grapio/GameController.java
@@ -12,24 +12,23 @@ import java.util.*;
 public class GameController implements Initializable {
 
     @FXML
-    private ImageView f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,
+    private ImageView f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,
             f11, f12, f13, f14, f15, f16, f17, f18, f19, f20,
             f21, f22, f23, f24, f25, f26, f27, f28, f29, f30,
             f31, f32, f33, f34, f35, f36, f37, f38, f39, f40,
             f41, f42, f43, f44, f45, f46, f47, f48, f49, f50,
-            f51, f52, f53, f54, f55, f56;
+            f51, f52, f53, f54, f55, f56, f57;
 
-    private final List<ImageView> fields = new ArrayList<>();
+    private final List<ImageView> fieldsImg = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Collections.addAll(fields, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,
+        Collections.addAll(fieldsImg, f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10,
                 f11, f12, f13, f14, f15, f16, f17, f18, f19, f20,
                 f21, f22, f23, f24, f25, f26, f27, f28, f29, f30,
                 f31, f32, f33, f34, f35, f36, f37, f38, f39, f40,
                 f41, f42, f43, f44, f45, f46, f47, f48, f49, f50,
-                f51, f52, f53, f54, f55, f56);
-
+                f51, f52, f53, f54, f55, f56, f57);
     }
 
     @FXML
@@ -40,7 +39,7 @@ public class GameController implements Initializable {
     public void throwDice() {
         int roll = dice.giveRandom(1, 6);
         diceLabel.setText(((Integer)roll).toString());
-        fields.get(roll).setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/players/1.png"))));
+        fieldsImg.get(roll).setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/players/1.png"))));
     }
 
 
--- src/main/java/com/example/grapio/MenuController.java
+++ src/main/java/com/example/grapio/MenuController.java
@@ -24,6 +24,24 @@ public class MenuController {
     }
 
     @FXML
+    protected void toOptions(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("options-view.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void toMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("menu-view.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
     protected void exit() {
         System.exit(0);
     }
